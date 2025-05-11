eea19217b656ea4936bece30a4152c1e89c07486
Hermon Getachew <hermongetachew10@gmail.com>
2025-05-11 13:48:02 +0300

    fix(compiler): add data processing from the stack big code dataset plus related accomodative changes

--- Compiler/main.py
+++ Compiler/main.py
@@ -3,15 +3,16 @@ from compiler import Compiler
 
 if __name__ == "__main__":
     ROOT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    DATA_DIRECTORY = os.path.join(ROOT_DIRECTORY, '_test_data')
+    DATA_DIRECTORY = os.path.join(ROOT_DIRECTORY, '_data')
 
     # Mapping of file extensions to language and compiler commands
     LANGUAGE_COMPILERS = {
-        '.c': ['/usr/local/llvm-10/bin/clang', '-S', '-emit-llvm'],
-        '.cpp': ['/usr/local/llvm-10/bin/clang', '-S', '-emit-llvm'],
+        '.c': ['/usr/local/llvm-10/bin/clang', '-S', '-emit-llvm', '-O0'],
+        '.cpp': ['/usr/local/llvm-10/bin/clang++', '-std=c++17', '-S', '-emit-llvm', '-O0'],
         '.swift': ['swiftc', '-emit-ir'],
         '.rs': ['rustc', '--emit=llvm-ir'],
     }
 
     compiler = Compiler(DATA_DIRECTORY, LANGUAGE_COMPILERS)
-    compiler.generate_ir()
+    # compiler.generate_ir()
+    compiler.generate_ir_from_big_code()
